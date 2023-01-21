c496dec414561717a642a915a82f49421a4f32b8
mlgranger540 <mattgranger540@outlook.com>
2023-01-21 12:48:39 +0000

    Add collapsible navbar for resolutions under 768px +add books and background image to p3-books.html

---
+++ assets/js/navbar.js
@@ -0,0 +1,11 @@
+/**
+ * Searches for an element with id collapsedContent, then toggles its display property between block and none on click
+ */
+function openNav(){
+    var collapsedNavbar = document.getElementById("collapsedContent")
+    if (collapsedNavbar.style.display === "block") {
+        collapsedNavbar.style.display = "none";
+    } else {
+        collapsedNavbar.style.display = "block";
+    }
+}
